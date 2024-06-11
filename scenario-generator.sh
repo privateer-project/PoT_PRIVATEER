d826f311ada1a5cad1086bfdb82f3b88326110cb
mattinelorza <mattinantartiko.elorzaforcada@telefonica.com>
2024-06-11 09:38:36 +0200

    Kafka support

--- scenario-generator.sh
+++ scenario-generator.sh
@@ -29,6 +29,7 @@ if  [ -n "$2" ] && [ $2 = "docker" ]; then
 			ip route add 10.1.1.0/24 via 10.1.2.2
 		fi
 	elif [ $1 = "controller" ]; then
+        sudo pip3 install confluent_kafka
 		parameters_generation/gen_keys.sh $2
 		sudo chmod 777 keys/ca.crt
 		sudo cp controller/switch.py /home/p4/tutorials/utils/p4runtime_lib/switch.py
@@ -38,7 +39,7 @@ if  [ -n "$2" ] && [ $2 = "docker" ]; then
 		p4c-bm2-ss --p4v 16 --p4runtime-files p4/pot.p4.p4info.txt -o p4/pot.json p4/pot.p4
 		sudo python3 controller/runtimev2.py --p4info p4/pot.p4.p4info.txt --bmv2-json p4/pot.json --ssl
 		sleep 20
-		sudo python3 controller/collector.py -c $2 &
+		sudo python3 controller/collector_kafka.py -c $2 &
 	else
 		sleep 10
 		sudo sysctl -w net.ipv4.ip_forward=0
